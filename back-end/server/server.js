171a3410de6b861a94945b488ae3860d19ce7895
Belarif <b.ocine@live.fr>
2024-07-05 17:33:55 +0200

    argent-bank-prod: fix-2 yaml file

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -3,11 +3,7 @@ const dotEnv = require("dotenv");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yamljs");
-const path = require("path");
-
-const swaggerDocs = yaml.load(
-  path.join(__dirname, "..", "public", "swagger.yaml")
-);
+const swaggerDocs = yaml.load("./swagger.yaml");
 const dbConnection = require("./database/connection");
 
 dotEnv.config();
@@ -29,9 +25,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 
 // API Documentation
-if (process.env.NODE_ENV === "production") {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-}
+// if (process.env.NODE_ENV !== "production") {
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// }
 
 app.get("/", (req, res, next) => {
   res.send("Hello from my Express server v2!");
