7f11a38836af8a988062684d2e87083270169858
Belarif <b.ocine@live.fr>
2024-07-05 16:30:52 +0200

    argenr-bank-prod: using module path to use absolute path

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -3,7 +3,9 @@ const dotEnv = require("dotenv");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yamljs");
-const swaggerDocs = yaml.load("./swagger.yaml");
+const path = require("path");
+
+const swaggerDocs = yaml.load(path.join(__dirname, "swagger.yaml"));
 const dbConnection = require("./database/connection");
 
 dotEnv.config();
