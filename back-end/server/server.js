5cdf239400d4507fef0326650febc6ffa2b40572
Belarif <b.ocine@live.fr>
2024-07-08 11:57:57 +0200

    argent-bank-prod: fix-4 swagger file

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -3,8 +3,7 @@ const dotEnv = require("dotenv");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yamljs");
-const path = require("path");
-const swaggerDocs = yaml.load(path.resolve(__dirname, "./swagger.yaml"));
+const swaggerDocs = yaml.load("./swagger.yaml");
 const dbConnection = require("./database/connection");
 
 dotEnv.config();
